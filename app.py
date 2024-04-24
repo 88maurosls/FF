b9aed412d1f3a16dca7a38688d3fa86c41c8899c
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 15:27:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import json
-from PIL import Image
-import io
-import os  # Aggiunta dell'importazione di os
 
 @st.cache(allow_output_mutation=True)
 def get_images_from_url(url):
@@ -36,21 +33,10 @@ def get_images_from_url(url):
         st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
         return []
 
-def download_and_convert_image(image_url):
-    try:
-        response = requests.get(image_url)
-        image = Image.open(io.BytesIO(response.content))
-        file_name = os.path.basename(image_url).split('?')[0] + ".jpg"
-        image.save(file_name, "JPEG")
-        st.success(f"Immagine convertita e salvata come: {file_name}")
-    except Exception as e:
-        st.error(f"Non è stato possibile convertire o salvare l'immagine {image_url}: {str(e)}")
-
 def show_images(image_urls):
     if image_urls:
         for url in image_urls:
             st.image(url, width=100)
-            download_and_convert_image(url)
     else:
         st.write("Nessuna immagine trovata.")
 
