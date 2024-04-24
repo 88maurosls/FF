e1b84eb2a45f869f4bf1b1ae4851da991e2f9d8b
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 15:04:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,34 +5,29 @@ import json
 import asyncio
 import aiohttp
 
-@st.cache(allow_output_mutation=True)
 def get_images_from_url(url):
-    try:
-        res = requests.get(url, headers={'user-agent': 'some agent'})
-        if res.status_code == 200:
-            soup = BeautifulSoup(res.content, 'html.parser')
-            script_data = soup.find('script', type='application/ld+json')
-            if script_data:
-                data = json.loads(script_data.text)
-                images = data.get('image')
-                image_urls = []
-                if images:
-                    if isinstance(images, list):
-                        for img in images:
-                            if isinstance(img, dict):
-                                image_urls.append(img.get('contentUrl'))
-                    else:
-                        if isinstance(images, dict):
-                            image_urls.append(images.get('contentUrl'))
-                return image_urls
-            else:
-                st.error("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
-                return []
+    res = requests.get(url, headers={'user-agent': 'some agent'})
+    if res.status_code == 200:
+        soup = BeautifulSoup(res.content, 'html.parser')
+        script_data = soup.find('script', type='application/ld+json')
+        if script_data:
+            data = json.loads(script_data.text)
+            images = data.get('image')
+            image_urls = []
+            if images:
+                if isinstance(images, list):
+                    for img in images:
+                        if isinstance(img, dict):
+                            image_urls.append(img.get('contentUrl'))
+                else:
+                    if isinstance(images, dict):
+                        image_urls.append(images.get('contentUrl'))
+            return image_urls
         else:
-            st.error(f"Errore HTTP: {res.status_code}")
+            st.error("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
             return []
-    except Exception as e:
-        st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
+    else:
+        st.error(f"Errore HTTP: {res.status_code}")
         return []
 
 async def download_image(session, url):
@@ -63,7 +58,7 @@ def main():
     if st.button("Scarica Immagini"):
         if codice:
             url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
-            image_urls = get_images_from_url(url)
+            image_urls = st.cache_resource(func=get_images_from_url, resource_url=url)
             if image_urls:
                 images_content = asyncio.run(download_images(image_urls))
                 show_images(images_content)
