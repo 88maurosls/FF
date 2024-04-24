d154a70473e5f9fc9cf7153ee8bcebddc6f6af5d
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 13:05:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import json
-from PIL import Image
-import io
-import base64
 
 # Funzione per ottenere le immagini dall'URL
 @st.cache(allow_output_mutation=True)
@@ -33,27 +30,12 @@ def get_images_from_url(url):
         st.error(f"Error retrieving images from URL: {str(e)}")
         return []
 
-# Funzione per convertire l'immagine in JPEG e codificarla in base64
-def convert_and_encode_image(image_data):
-    image = Image.open(io.BytesIO(image_data))
-    if image.format == 'WEBP':
-        image = image.convert('RGB')
-    img_buffer = io.BytesIO()
-    image.save(img_buffer, format="JPEG")
-    encoded_image = base64.b64encode(img_buffer.getvalue()).decode()
-    return encoded_image
-
 # Interfaccia utente Streamlit
 codice = st.text_input("Inserisci l'ID Farfetch:", "")
 if st.button("Scarica Immagini"):
     if codice:
         url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
         image_urls = get_images_from_url(url)
-        for url in image_urls:
-            st.image(url, width=300)
-            if st.button("Convert & Download", key=url):
-                with st.spinner('Processing image...'):
-                    response = requests.get(url)
-                    encoded_image = convert_and_encode_image(response.content)
-                    href = f'<a href="data:image/jpeg;base64,{encoded_image}" download="image.jpg">Download Image</a>'
-                    st.markdown(href, unsafe_allow_html=True)
+        for idx, url in enumerate(image_urls, start=1):
+            st.image(url, width=300, caption=f"Immagine {idx}")
+            st.markdown(f"[Scarica Immagine {idx}]({url})")
