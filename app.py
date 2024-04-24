f4dcaffc03a854478171ffc3e7734d42df66fc72
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 14:44:06 +0200

    Update app.py

--- app.py
+++ app.py
@@ -35,22 +35,18 @@ def get_images_from_url(url):
         st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
         return []
 
-async def download_image(session, url, width=100):
+async def download_image(session, url):
     try:
         async with session.get(url) as response:
             image_content = await response.read()
-            st.image(image_content, caption='Immagine', width=width)
+            st.image(image_content, caption='Immagine', use_column_width=True)
     except Exception as e:
         st.error(f"Errore durante il download dell'immagine: {str(e)}")
 
-
-async def download_images(image_urls, columns, width=100):
+async def download_images(image_urls):
     async with aiohttp.ClientSession() as session:
-        tasks = [download_image(session, url, width) for url in image_urls]
-        for task, column in zip(tasks, columns):
-            await task
-            with column:
-                pass
+        tasks = [download_image(session, url) for url in image_urls]
+        await asyncio.gather(*tasks)
 
 def main():
     st.title("Downloader di Immagini da Farfetch")
@@ -60,17 +56,9 @@ def main():
             url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
             image_urls = get_images_from_url(url)
             if image_urls:
-                images_per_row = 3  # Numero di immagini per riga
-                rows = [st.empty() for _ in range(len(image_urls) // images_per_row + 1)]
-                for i, url in enumerate(image_urls):
-                    row_index = i // images_per_row
-                    with rows[row_index]:
-                        st.image(url, caption=f"Immagine {i+1}", width=100)
+                asyncio.run(download_images(image_urls))
             else:
                 st.write("Nessuna immagine trovata.")
 
 if __name__ == "__main__":
     main()
-
-
-
