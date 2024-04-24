253026ba625372d04c07c9f0b1b03a03a105d7be
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 17:34:30 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import json
 from PIL import Image
 import io
 from rembg import remove
-import cv2
 
 # Funzione per scaricare e memorizzare in cache le immagini
 @st.cache(allow_output_mutation=True, suppress_st_warning=True, max_entries=20, ttl=3600)
@@ -25,14 +24,14 @@ def download_image(url):
 # Funzione per convertire le immagini in formato JPEG utilizzando rembg
 # Funzione per convertire le immagini in formato JPEG utilizzando rembg
 # Funzione per convertire le immagini in formato JPEG utilizzando rembg
-def convert_to_jpeg(image):
+# Funzione per convertire le immagini in formato JPEG utilizzando rembg
+def convert_to_jpeg(image, quality=95, noise_level=0):
     with io.BytesIO() as output:
         image.save(output, format="PNG")
         png_data = output.getvalue()
-    # Carica l'immagine utilizzando OpenCV e salvala senza alcuna trasformazione
-    img_np = cv2.imdecode(np.frombuffer(png_data, np.uint8), cv2.IMREAD_UNCHANGED)
-    _, buf = cv2.imencode('.jpg', img_np)
-    return Image.open(io.BytesIO(buf))
+    jpeg_data = remove(png_data, quality=quality, noise_level=noise_level)
+    return Image.open(io.BytesIO(jpeg_data))
+
 
 
 
