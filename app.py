94bdd4a3596ebd396c2420d11a32ee7801584e7a
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 14:45:55 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import json
-import asyncio
-import aiohttp
+import os
+import urllib.request
+import base64
 
 @st.cache(allow_output_mutation=True)
 def get_images_from_url(url):
@@ -35,19 +36,6 @@ def get_images_from_url(url):
         st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
         return []
 
-async def download_image(session, url):
-    try:
-        async with session.get(url) as response:
-            image_content = await response.read()
-            st.image(image_content, caption='Immagine', use_column_width=True)
-    except Exception as e:
-        st.error(f"Errore durante il download dell'immagine: {str(e)}")
-
-async def download_images(image_urls):
-    async with aiohttp.ClientSession() as session:
-        tasks = [download_image(session, url) for url in image_urls]
-        await asyncio.gather(*tasks)
-
 def main():
     st.title("Downloader di Immagini da Farfetch")
     codice = st.text_input("Inserisci l'ID Farfetch:", "")
@@ -56,9 +44,17 @@ def main():
             url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
             image_urls = get_images_from_url(url)
             if image_urls:
-                asyncio.run(download_images(image_urls))
-            else:
-                st.write("Nessuna immagine trovata.")
+                for i, url in enumerate(image_urls, start=1):
+                    st.write(f"Immagine {i}:")
+                    st.image(url, use_column_width=True)
+                    download_button_label = f"Scarica Immagine {i}"
+                    download_image(url, download_button_label)
+
+def download_image(url, button_label):
+    image_content = urllib.request.urlopen(url).read()
+    base64_image = base64.b64encode(image_content).decode('utf-8')
+    href = f'<a href="data:image/jpeg;base64,{base64_image}" download="{os.path.basename(url)}">{button_label}</a>'
+    st.markdown(href, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
