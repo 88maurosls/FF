95a776e4b9ea8cef259273daa9b1cb73706fe900
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 14:30:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from bs4 import BeautifulSoup
 import json
 import os
 import urllib.request
-import base64
 
 @st.cache(allow_output_mutation=True)
 def get_images_from_url(url):
@@ -21,10 +20,14 @@ def get_images_from_url(url):
                     if isinstance(images, list):
                         for img in images:
                             if isinstance(img, dict):
-                                image_urls.append(img.get('contentUrl'))
+                                thumbnail = img.get('thumbnailUrl')
+                                if thumbnail:
+                                    image_urls.append(thumbnail)
                     else:
                         if isinstance(images, dict):
-                            image_urls.append(images.get('contentUrl'))
+                            thumbnail = images.get('thumbnailUrl')
+                            if thumbnail:
+                                image_urls.append(thumbnail)
                 return image_urls
             else:
                 st.error("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
@@ -37,24 +40,16 @@ def get_images_from_url(url):
         return []
 
 def main():
-    st.title("Downloader di Immagini da Farfetch")
+    st.title("Downloader di Anteprime Immagini da Farfetch")
     codice = st.text_input("Inserisci l'ID Farfetch:", "")
-    if st.button("Scarica Immagini"):
+    if st.button("Mostra Anteprime"):
         if codice:
             url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
             image_urls = get_images_from_url(url)
             if image_urls:
                 for i, url in enumerate(image_urls, start=1):
-                    st.write(f"Immagine {i}:")
+                    st.write(f"Anteprima {i}:")
                     st.image(url, use_column_width=True)
-                    download_button_label = f"Scarica Immagine {i}"
-                    download_image(url, download_button_label)
-
-def download_image(url, button_label):
-    image_content = urllib.request.urlopen(url).read()
-    base64_image = base64.b64encode(image_content).decode('utf-8')
-    href = f'<a href="data:image/jpeg;base64,{base64_image}" download="{os.path.basename(url)}">{button_label}</a>'
-    st.markdown(href, unsafe_allow_html=True)
 
 if __name__ == "__main__":
     main()
