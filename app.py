00f6c6efd6c4d7de6dcf7087475b39ee67fbb2c8
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 14:37:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,10 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import json
-import os
-import urllib.request
-import base64
+import asyncio
+import aiohttp
 
+@st.cache(allow_output_mutation=True)
 def get_images_from_url(url):
     try:
         res = requests.get(url, headers={'user-agent': 'some agent'})
@@ -35,6 +35,19 @@ def get_images_from_url(url):
         st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
         return []
 
+async def download_image(session, url):
+    try:
+        async with session.get(url) as response:
+            image_content = await response.read()
+            st.image(image_content, caption='Immagine', use_column_width=True)
+    except Exception as e:
+        st.error(f"Errore durante il download dell'immagine: {str(e)}")
+
+async def download_images(image_urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = [download_image(session, url) for url in image_urls]
+        await asyncio.gather(*tasks)
+
 def main():
     st.title("Downloader di Immagini da Farfetch")
     codice = st.text_input("Inserisci l'ID Farfetch:", "")
@@ -43,13 +56,9 @@ def main():
             url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
             image_urls = get_images_from_url(url)
             if image_urls:
-                for i, url in enumerate(image_urls, start=1):
-                    st.write(f"Immagine {i}:")
-                    show_image(url, i)
-
-def show_image(url, index):
-    image_content = urllib.request.urlopen(url).read()
-    st.image(image_content, caption=f"Immagine {index}", use_column_width=True)
+                asyncio.run(download_images(image_urls))
+            else:
+                st.write("Nessuna immagine trovata.")
 
 if __name__ == "__main__":
     main()
