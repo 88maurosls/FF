12b242ec5e579ecb7b714e9a0d3b5c159a022a25
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 17:24:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import json
 from PIL import Image
 import io
+from rembg import remove
 
 # Funzione per scaricare e memorizzare in cache le immagini
 @st.cache(allow_output_mutation=True, suppress_st_warning=True, max_entries=20, ttl=3600)
@@ -20,11 +21,12 @@ def download_image(url):
         st.warning(f"Errore durante il download dell'immagine: {str(e)}")
         return None
 
-# Funzione per convertire le immagini in formato JPEG
+# Funzione per convertire le immagini in formato JPEG utilizzando rembg
 def convert_to_jpeg(image):
     with io.BytesIO() as output:
-        image.save(output, format="JPEG")
-        jpeg_data = output.getvalue()
+        image.save(output, format="PNG")
+        png_data = output.getvalue()
+    jpeg_data = remove(png_data)
     return Image.open(io.BytesIO(jpeg_data))
 
 # Funzione per ottenere le immagini dall'URL
@@ -39,4 +41,48 @@ def get_images_from_url(url):
                 data = json.loads(script_data.text)
                 images = data.get('image')
                 image_urls = []
-                if
+                if images:
+                    if isinstance(images, list):
+                        for img in images:
+                            if isinstance(img, dict):
+                                image_urls.append(img.get('contentUrl'))
+                    else:
+                        if isinstance(images, dict):
+                            image_urls.append(images.get('contentUrl'))
+                return image_urls
+            else:
+                st.warning("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
+                return []
+        else:
+            st.warning(f"Errore HTTP: {response.status_code}")
+            return []
+    except Exception as e:
+        st.warning(f"Errore durante il recupero delle immagini dall'URL: {str(e)}")
+        return []
+
+# Funzione per visualizzare le immagini
+def show_images(image_urls):
+    if image_urls:
+        for url in image_urls:
+            st.subheader("Immagine")
+            image = download_image(url)
+            if image:
+                jpeg_image = convert_to_jpeg(image)
+                st.image(jpeg_image, use_column_width=True, caption='Immagine in formato JPEG')
+    else:
+        st.warning("Nessuna immagine trovata.")
+
+# Funzione principale
+def main():
+    st.title("Scarica Immagini da Farfetch")
+
+    codice = st.text_input("Inserisci l'ID Farfetch:", "")
+
+    if st.button("Scarica Immagini"):
+        if codice:
+            url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
+            image_urls = get_images_from_url(url)
+            show_images(image_urls)
+
+if __name__ == "__main__":
+    main()
