053bfe880822b46407568ee42ea78f24ccd50002
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-04-24 12:00:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,10 +2,6 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import json
-import os
-
-# Assicurati che la cartella per le immagini esista
-os.makedirs('downloaded_images', exist_ok=True)
 
 @st.cache(allow_output_mutation=True)
 def get_images_from_url(url):
@@ -37,23 +33,10 @@ def get_images_from_url(url):
         st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
         return []
 
-def download_image(url):
-    filename = url.split('/')[-1]
-    filepath = os.path.join('downloaded_images', filename)
-    if not os.path.exists(filepath):  # Evita il re-download se già esiste
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            with open(filepath, 'wb') as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    f.write(chunk)
-    return filepath
-
 def show_images(image_urls):
     if image_urls:
         for url in image_urls:
             st.image(url, width=100)
-            download_path = download_image(url)  # Scarica l'immagine
-            st.download_button(label="Scarica", data=open(download_path, "rb"), file_name=os.path.basename(download_path), mime='image/jpeg')
     else:
         st.write("Nessuna immagine trovata.")
 
