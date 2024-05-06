d3136790b795e6e9dd38ab3198fa37ec2a135a85
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-05-06 11:35:59 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10,30 +10,36 @@ def get_images_from_url(url):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'}
     res = requests.get(url, headers=headers)
     if res.status_code == 200:
+        st.write("Connessione riuscita, analisi del contenuto HTML in corso...")
         soup = BeautifulSoup(res.content, 'html.parser')
         script_data = soup.find('script', type='application/ld+json')
         if script_data:
+            st.write("Script JSON trovato, estrazione delle immagini...")
             data = json.loads(script_data.text)
             images = data.get('image')
             if images:
                 return images if isinstance(images, list) else [images]
+            else:
+                st.error("Nessuna immagine trovata nel JSON.")
         else:
             st.error("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
     else:
-        st.error(f"Errore HTTP: {res.status_code}")
-    return []
+        st.error(f"Errore HTTP: {res.status_code} - Fallito il tentativo di connessione all'URL fornito.")
 
+    return []
 
 def show_images(images):
     if images:
         for img in images:
             if isinstance(img, dict):
                 img_url = img.get('contentUrl')
+                st.write(f"Mostra immagine all'URL: {img_url}")
                 st.image(img_url, width=100, caption=img_url)
     else:
-        st.write("Nessuna immagine trovata.")
+        st.write("Nessuna immagine trovata per la visualizzazione.")
 
 def download_image(url):
+    st.write(f"Tentativo di download dall'URL: {url}")
     response = requests.get(url)
     if response.status_code == 200:
         img = Image.open(BytesIO(response.content))
@@ -45,11 +51,14 @@ def download_image(url):
                 file_name='downloaded_image.png',
                 mime="image/png"
             )
+    else:
+        st.error(f"Impossibile scaricare l'immagine. Errore HTTP: {response.status_code}")
 
 codice = st.text_input("Inserisci l'ID Farfetch:", "")
 if st.button("Scarica Immagini"):
     if codice:
         url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
+        st.write(f"URL generato: {url}")
         images = get_images_from_url(url)
         show_images(images)
         if st.button("Download Selected Image"):
