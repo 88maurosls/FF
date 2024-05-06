f38dcee7e3faa74ee0f068ab9f5764ffad40973a
88maurosls <162556108+88maurosls@users.noreply.github.com>
2024-05-06 11:39:36 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,67 +2,46 @@ import streamlit as st
 import requests
 from bs4 import BeautifulSoup
 import json
-from PIL import Image
-from io import BytesIO
 
-session = requests.Session()
-session.headers.update({'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'})
-
-@st.cache_resource
 def get_images_from_url(url):
-    res = session.get(url)
-    if res.status_code == 200:
-        soup = BeautifulSoup(res.content, 'html.parser')
-        script_data = soup.find('script', type='application/ld+json')
-        if script_data:
-            data = json.loads(script_data.text)
-            images = data.get('image')
-            if images:
-                return images if isinstance(images, list) else [images]
+    try:
+        res = requests.get(url, headers={'user-agent': 'some agent'})
+        if res.status_code == 200:
+            soup = BeautifulSoup(res.content, 'html.parser')
+            script_data = soup.find('script', type='application/ld+json')
+            if script_data:
+                data = json.loads(script_data.text)
+                images = data.get('image')
+                image_urls = []
+                if images:
+                    if isinstance(images, list):
+                        for img in images:
+                            if isinstance(img, dict):
+                                image_urls.append(img.get('contentUrl'))
+                    else:
+                        if isinstance(images, dict):
+                            image_urls.append(images.get('contentUrl'))
+                return image_urls
+            else:
+                st.error("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
+                return []
         else:
-            st.error("Nessun script di tipo 'application/ld+json' trovato nel contenuto HTML.")
+            st.error(f"Errore HTTP: {res.status_code}")
+            return []
+    except Exception as e:
+        st.error(f"Errore durante il tentativo di recupero delle immagini dall'URL: {str(e)}")
+        return []
+
+def show_images(image_urls):
+    if image_urls:
+        for url in image_urls:
+            st.image(url, width=100)
     else:
-        st.error(f"Errore HTTP: {res.status_code} - Fallito il tentativo di connessione all'URL fornito.")
-    return []
-
-
-
-
-def show_images(images):
-    if images:
-        for img in images:
-            if isinstance(img, dict):
-                img_url = img.get('contentUrl')
-                st.write(f"Mostra immagine all'URL: {img_url}")
-                st.image(img_url, width=100, caption=img_url)
-    else:
-        st.write("Nessuna immagine trovata per la visualizzazione.")
-
-def download_image(url):
-    st.write(f"Tentativo di download dall'URL: {url}")
-    response = requests.get(url)
-    if response.status_code == 200:
-        img = Image.open(BytesIO(response.content))
-        img.save(f'temp_image.png')
-        with open('temp_image.png', "rb") as file:
-            st.download_button(
-                label="Download Image",
-                data=file,
-                file_name='downloaded_image.png',
-                mime="image/png"
-            )
-    else:
-        st.error(f"Impossibile scaricare l'immagine. Errore HTTP: {response.status_code}")
+        st.write("Nessuna immagine trovata.")
 
 codice = st.text_input("Inserisci l'ID Farfetch:", "")
 if st.button("Scarica Immagini"):
     if codice:
         url = f'https://www.farfetch.com/shopping/item{codice}.aspx'
-        st.write(f"URL generato: {url}")
-        images = get_images_from_url(url)
-        show_images(images)
-        if st.button("Download Selected Image"):
-            for image in images:
-                if isinstance(image, dict):
-                    img_url = image.get('contentUrl')
-                    download_image(img_url)
+        image_urls = get_images_from_url(url)
+        show_images(image_urls)
